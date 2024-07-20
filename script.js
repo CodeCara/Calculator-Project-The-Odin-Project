67479ff1e48f659e7c501dae11d5334add990f39
CodeCara <149534506+CodeCara@users.noreply.github.com>
2024-07-20 10:02:13 +0200

    Mathematical functions and operate function set up (operate function not fully operational yet as will requirs inputs plus some further tweaking at a later point

---
+++ script.js
@@ -0,0 +1,40 @@
+
+//GLOBAL FUNCTIONS
+function add(a,b){
+console.log(a+b);
+return (a+b);
+}
+
+function subtract(a,b){
+    console.log(a-b);
+    return (a-b);
+}
+
+function divide(a,b){
+    console.log(a/b);
+    return (a/b);
+ ;
+}
+
+function multiply(a,b){
+    console.log(a*b);
+    return(a*b);
+
+}
+
+
+
+
+
+//function operate is not working properly YET
+function operate () {
+let firstNumber = " ";//" " assign emp strings later
+let operator = " ";//might need to figure out a way to destringify
+let secondNumber = " "; 
+ let result = " "; //firstNumber+operator+secondNumber HOW TO PUT THIS TOGETHER.  template literal?
+console.log(result);
+return result;
+
+
+}
+console.log(operate());
