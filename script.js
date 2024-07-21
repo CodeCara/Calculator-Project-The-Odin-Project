7914eb70b182563994212e7c159ed196f645f9aa
CodeCara <149534506+CodeCara@users.noreply.github.com>
2024-07-21 08:01:27 +0200

    Button and display variables created

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-//GLOBAL FUNCTIONS
+//Mathematical functions
 function add(a,b){
 console.log(a+b);
 return (a+b);
@@ -26,7 +26,7 @@ function multiply(a,b){
 
 
 
-//function operate is not working properly YET
+//Operate function is not working properly YET
 function operate () {
 let firstNumber = " ";//" " assign emp strings later
 let operator = " ";//might need to figure out a way to destringify
@@ -38,3 +38,28 @@ return result;
 
 }
 console.log(operate());
+
+//Number buttons allocated to variables
+const one = document.querySelector('.one-btn');
+const two = document.querySelector('.two-btn');
+const three = document.querySelector('.three-btn');
+const four = document.querySelector('.four-btn');
+const five = document.querySelector('.five-btn');
+const six = document.querySelector('.six-btn');
+const seven = document.querySelector('.seven-btn');
+const eight = document.querySelector('.eight-btn');
+const nine = document.querySelector('.nine-btn');
+
+//Display variable
+let display = document.querySelector('.display');
+display.innerText= "hi";
+
+//Remaining buttons variables
+const add = document.querySelector('.add-btn');
+const subtract = document.querySelector('.subtract-btn');
+const equals = document.querySelector('.equals-btn"');
+const decimalPoint = document.querySelector('.decimal-point-btn');
+const clear = document.querySelector('.clear-btn"');
+
+
+
